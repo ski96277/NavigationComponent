c2f3090c541ce5bb77c200c9f819317828e86f2a
ski96277 <imran.bu96@gmail.com>
2020-10-27 00:59:11 -0700

    menu item added

--- app/src/main/java/live/qtech/navigationcomponent/MainActivity.kt
+++ app/src/main/java/live/qtech/navigationcomponent/MainActivity.kt
@@ -1,10 +1,13 @@
 package live.qtech.navigationcomponent
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -24,5 +27,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId==R.id.fragment_tarms){
+
+            val action = NavGraphDirections.actionGlobalTarmsFragment()
+            navController.navigate(action)
+
+            true
+        }else{
+            item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        }
     }
 }
